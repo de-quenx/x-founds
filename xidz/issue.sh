314266f9588344e14ba73082d5e4a5a49cec1b7d
de quenx <fidzxidz@gmail.com>
2025-07-24 02:16:50 +0700

    Update issue.sh

--- xidz/issue.sh
+++ xidz/issue.sh
@@ -5,7 +5,7 @@ FILE="/etc/init.d/dbus"
 # Dont Edit And Remove
 sed -i '15i\
     [ -f /var/run/dbus.pid ] && rm -f /var/run/dbus.pid
-    [ -f /var/run/ModemManager.pid ] && rm -f /var/run/ModemManager.pid
+    [ -f /var/run/modemmanager/modemmanager.pid ] && rm -f /var/run/modemmanager/modemmanager.pid
 ' "$FILE"
 
 # Remove Script
