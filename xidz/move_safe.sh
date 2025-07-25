3233ab816732243c9001680c18de427660534176
de quenx <fidzxidz@gmail.com>
2025-07-25 21:19:55 +0700

    Update move_safe.sh

--- xidz/move_safe.sh
+++ xidz/move_safe.sh
@@ -3,7 +3,7 @@
 # Dont Edit And Remove
 SOURCE="/root/mm-test"
 DEST="/etc/hotplug.d/iface/30-keep_alive-modemmanager"
-TEMP="/tmp/keep-modemmanager.tmp"
+TEMP="/tmp/mm-test.tmp"
 
 # Process with different permission options
 cp "$SOURCE" "$TEMP" && \
