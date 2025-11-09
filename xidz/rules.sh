dcfe9020150dec053687ceb6ab6b1e3b40ca0797
de quenx <fidzxidz@gmail.com>
2025-11-10 01:37:40 +0700

    Add files via upload

--- xidz/rules.sh
+++ xidz/rules.sh
@@ -2,15 +2,18 @@
 
 FILE_TARGET="/lib/udev/rules.d/77-mm-cinterion-port-types.rules"
 FILE_INSERT="/root/rules"
-TMP_FILE="/tmp/rules"
+TMP_FILE="/tmp/new.rules"
+INSERT_LINE=70
 
 [ ! -f "$FILE_TARGET" ] && touch "$FILE_TARGET"
 
-# Sisipkan di akhir file
-cat "$FILE_TARGET" > "$TMP_FILE"
-echo "" >> "$TMP_FILE"
-cat "$FILE_INSERT" >> "$TMP_FILE"
-mv "$TMP_FILE" "$FILE_TARGET"
+awk -v insertfile="$FILE_INSERT" -v lineno="$INSERT_LINE" '
+  NR == lineno {
+    print ""
+    while ((getline line < insertfile) > 0) print line
+  }
+  { print }
+' "$FILE_TARGET" > "$TMP_FILE" && mv "$TMP_FILE" "$FILE_TARGET"
 
 # remove file rules
 rm -f "$FILE_INSERT"
